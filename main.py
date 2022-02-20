139552c7d120100fa331aa5fecab07d9d54ccbb5
mrTokar <85022291+mrTokar@users.noreply.github.com>
2022-02-20 11:20:34 +0300

    Scroll update
    - Add Scroll_x
    - Change data input. Now it is a separate function
    - Add new parameter d. This is the distance from the laser source to the first lens
    - Add choosing input a. Before asking a, it will now be asked how this should be done. User input or calculation.
    - Calculation a, through the tangent of this angle (tg(a) = y/d)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from tkinter import Tk, Canvas, Frame
-from tkinter import BOTH
+from cmath import atan, pi
+from tkinter import Label, Tk, Canvas, Frame, Scrollbar
+from tkinter import BOTH, HORIZONTAL, ALL, NW, N, W
 from math import tan, radians
 
 def solution_of_the_equation(k, point) -> tuple:
@@ -8,17 +9,19 @@ def solution_of_the_equation(k, point) -> tuple:
     return (k, point[1] - k * point[0])
 
 class Lens:
-    def __init__(self, canvas, x, y, F):
+    def __init__(self, canvas, x, y, F, number):
+        l = 35 * 5 # длинна линзы
         self.up = y # y-координата верха линзы
-        self.center = (x, y+87.5) 
-        self.down = y+175 # y-координата низа линзы
+        self.center = (x, y + (l / 2)) 
+        self.down = y + l # y-координата низа линзы
         self.F = self.center[0] + F
-        canvas.create_line(x, self.up, x, self.down) # сохдание линзы
+        canvas.create_line(x, self.up, x, self.down) # создание линзы
+        #Label(canvas, text=number).place(x= x, y= y-10)
         # ----- оформление линзы ------ 
         canvas.create_line((x - 10, self.up + 10), (x, self.up), (x + 10, self.up + 10))
         canvas.create_line((x - 10, self.down - 10), (x, self.down), (x + 10, self.down - 10))
 
-    def get_intersection(self, y= None, line= None):
+    def get_intersection(self, y= None, line= None) -> tuple:
         '''Находит точку пересечения лазера с линзой
         Если передан аргуемент y (Наколько выше центра падает лазер), то считает по нему
         Если передан аргумент line (ур-е прямой), то находит точку пересечения с помощьею него '''
@@ -27,8 +30,8 @@ class Lens:
         if line:
             return (self.center[0], line[0] * self.center[0] + line[1])
 
-    def refraction_lazer(self, line, point):
-        '''Вычисляет и возвращает ур-е приломленной прямой'''
+    def refraction_lazer(self, line, point) -> tuple:
+        '''Взовращает ур-е приломленной прямой''' 
         side_axis = solution_of_the_equation(line[0], self.center) # побочная оптическая ось
         f1 = side_axis[0] * self.F + side_axis[1] # f1 = k*F + b, проекция на ось y побочного фокуса
         k = (point[1] - f1)/(point[0] - self.F) # k = (ya - yf)/(xa - xf)
@@ -36,56 +39,83 @@ class Lens:
         return (line[0], line[1], (self.F, f1))
 
 
-class Example(Frame):
- 
+class MyApp(Tk):
+
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.scroll_x = Scrollbar(self, orient=HORIZONTAL)
+        self.canvas = Construction(self, xscrollcommand=self.scroll_x.set)
+        self.scroll_x.config(command=self.canvas.xview)
+        self.canvas.initUI()
+        self.frame = Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.frame,
+                                  anchor=N + W)
+
+        self.scroll_x.grid(row=0, column=0, sticky="we")
+        self.canvas.grid(row=1, column=0, sticky="nswe")
+        
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.bind("<Configure>", self.resize)
+        self.update_idletasks()
+        self.minsize(1000, 400)
+        self.attributes("-topmost", True)
+        self.attributes("-topmost", False)
+    
+    def resize(self, event):
+        region = self.canvas.bbox(ALL)
+        self.canvas.configure(scrollregion=region)
+
+
+class Construction(Canvas):
  
     def initUI(self):
-        self.master.title("Скамья линз")
-        self.pack(fill=BOTH, expand=1)
-
-        self.canvas = Canvas(self)
-
         lenses = []
-        pad_x = 40
+        pad_x, y, a, n, f, l = data_input()
         pad_y = 10
-        l = int(input("L в мм: ")) * 5
-        f = int(input("F в мм: ")) * 5
-        for _ in range(int(input("N: "))):
-            lenses.append(Lens(self.canvas, pad_x, pad_y, f))
+        for j in range(n):
+            lenses.append(Lens(self, pad_x, pad_y, f, j+1))
             pad_x += l
 
-        for i in range(len(lenses)):
+        for i in range(n):
             if i == 0:
-                pointA = lenses[0].get_intersection(y= int(input("y в мм: ")) * 5)
-                a = int(input("a в градусах: ")) #alpha
+                pointA = lenses[0].get_intersection(y)                
                 old_lazer = solution_of_the_equation(-tan(radians(a)), pointA) # (k, b) for y = kx + b
-                self.canvas.create_line((0, old_lazer[1]), pointA, fill='red') # рисовка запускаемого лазера
+                self.create_line((0, old_lazer[1]), pointA, fill='red') # рисовка запускаемого лазера
             new_lazer = lenses[i].refraction_lazer(old_lazer, pointA)
             # если в скамье есть еще линзы, то рисуем лазер до нее, иначе до фокуса линзы
-            if i + 1 <= len(lenses)-1:
+            if i + 1 <= n-1:
                 pointB = lenses[i+1].get_intersection(line=new_lazer)
                 # если лазер попадает в линзу
                 if lenses[i+1].up <= pointB[1] <= lenses[i+1].down: 
-                    self.canvas.create_line(pointA, pointB, fill='red')
+                    self.create_line(pointA, pointB, fill='red')
                     pointA, pointB = pointB, (0,0)
                     old_lazer = new_lazer
                 else:
-                    self.canvas.create_line(pointA, new_lazer[2], fill='red')
+                    self.create_line(pointA, pointB, fill='red')
                     break
         else:
-            self.canvas.create_line(pointA, new_lazer[2], fill='red')
+            self.create_line(pointA, new_lazer[2], fill='red')
 
-        self.canvas.create_line((0, lenses[-1].center[1]), (lenses[-1].F, lenses[-1].center[1]), dash= (10, 5))
-        self.canvas.pack(fill=BOTH, expand=1)
+        self.create_line((0, lenses[-1].center[1]), (lenses[-1].F, lenses[-1].center[1]), dash= (10, 5))
  
  
+def data_input():
+    d = float(input("d в мм: ")) * 5
+    y = float(input("y в мм: ")) * 5
+    if input('Задать а? (y/N)') in ('y', 'Y'):
+        a = float(input("a в градусах: ")) 
+    else:
+        a = float(str(atan(y / d) * 180 / pi)[1:-4])
+        print("ЗАДАННО a=", a)
+    n = int(input("N: "))
+    f = int(input("F в мм: ")) * 5
+    l = float(input("L в мм: ")) * 5
+    return d, y, a, n, f, l
+
+
 def main():
-    root = Tk()
-    ex = Example()
-    root.geometry("1000x400")
+    root = MyApp()
     root.mainloop()
  
  
